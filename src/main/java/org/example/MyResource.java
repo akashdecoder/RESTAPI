8a50bc85bb90a6ca345059a0551ac83b5a133fd4
akashdecoder <akash.ranjan1999@gmail.com>
2021-10-27 18:09:19 +0530

    First Commit: Jersey Project using Hibernate

---
+++ src/main/java/org/example/MyResource.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.example.dao.Employee;
+import org.example.dao.EmployeeDAO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+@Path("myresource")
+public class MyResource {
+
+    @GET
+    @Produces("application/json")
+    public List<Employee> getEmployee() {
+        EmployeeDAO dao = new EmployeeDAO();
+        List employees = dao.getEmployees();
+        return employees;
+    }
+
+
+    @POST
+    @Path("/create")
+    @Consumes("application/json")
+    public Response addEmployee(Employee emp){
+        emp.setName(emp.getName());
+        emp.setAge(emp.getAge());
+
+        EmployeeDAO dao = new EmployeeDAO();
+        dao.addEmployee(emp);
+
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("/update/{id}")
+    @Consumes("application/json")
+    public Response updateEmployee(@PathParam("id") int id, Employee emp){
+        EmployeeDAO dao = new EmployeeDAO();
+        int count = dao.updateEmployee(id, emp);
+        if(count==0){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    @Consumes("application/json")
+    public Response deleteEmployee(@PathParam("id") int id){
+        EmployeeDAO dao = new EmployeeDAO();
+        int count = dao.deleteEmployee(id);
+        if(count==0){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+}
